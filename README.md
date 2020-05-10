60af5082639d538a8d7c3964de24d05a3fd63933
Joshua Cadavez <cadav001@umn.edu>
2020-05-10 19:26:24 -0400

    Add Soft Asserts, Select, and Explicit Waits

--- README.md
+++ README.md
@@ -5,4 +5,12 @@
 ## Step 0: Setup
 1. Add Selenium and TestNG dependencies to the [*pom.xml*](https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java)
 2. Add the TestNG Eclipse Plugin
-3. Run a test to see if it's working. I used [Shoprite's Survey page](https://www.myshopriteexperience.com/)
+3. Run a test to see if it's working. I checked if it navigates to [Shoprite's Survey page](https://www.myshopriteexperience.com/)
+
+## Step 1: Soft Asserts, Select, and Explicit Waits
+0. I created a static Java file for my user inputs for Shoprite. But, kept it hidden from GitHUb for my data privacy.
+1. Implement [Soft Asserts](https://www.softwaretestingmaterial.com/soft-assert/) so you can check multiple items.
+2. Implement [Explicit Waits](https://www.selenium.dev/documentation/en/webdriver/waits/) with Expected Conditions and WebDriverWaits. 
+	- This will ensure that you will start testing once the elements or properties display after loading.
+3. Implement [Select](https://stackoverflow.com/questions/12940592/how-to-select-an-item-from-a-dropdown-list-using-selenium-webdriver-with-java) for inputting and checking Dropdown values
+	- This specifically helps for checking dropdown items.
--- src/test/java/java_maven_testng_shoprite/shoprite/TestLandingPage.java
+++ src/test/java/java_maven_testng_shoprite/shoprite/TestLandingPage.java
@@ -1,17 +1,86 @@
 package java_maven_testng_shoprite.shoprite;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java_maven_testng_shoprite.shoprite.util.UtilInput;
+
 import static org.testng.Assert.assertEquals;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestLandingPage {
 	WebDriver driver;
-	@Test
-	public void openShopritePage() throws InterruptedException {
+	WebDriverWait wait; // creates a wait until web element is found
+	
+	@BeforeClass
+	public void setUp() {
 		driver = new ChromeDriver();
-		driver.get("https://www.myshopriteexperience.com/");
-		Thread.sleep(3000L);
+		wait = new WebDriverWait(driver, 10); // 10 second wait
 	}
+	
+	@Test
+	public void openShopritePageTest() throws InterruptedException{
+		driver.get("https://www.myshopriteexperience.com/");
+		wait_for_start_btn();
+		WebElement startBtn = driver.findElement(By.id("NextButton"));
+		Assert.assertNotNull(startBtn, "WHEN you navigate to 'www.myshopriteexperience.com', "
+				+ "THEN you SHOULD see the Start Btn.");
+	}
+	
+	@Test(dependsOnMethods = { "openShopritePageTest" })
+	public void typeDateInputTest() throws InterruptedException{
+		WebElement input_date = driver.findElement(By.id("Index_VisitDateDatePicker"));
+		input_date.click();
+		String day = "//a[contains(text(),'"+UtilInput.day+"')]";
+		WebElement input_day = driver.findElement(By.xpath(day));
+		input_day.click();
+		WebElement input_date_value = driver.findElement(By.id("Index_VisitDateFormattedDate"));
+		String dateChosen = input_date_value.getAttribute("value");
+		wait.until(ExpectedConditions.attributeToBeNotEmpty(input_date_value, "value"));
+		Assert.assertEquals(dateChosen, UtilInput.fullDate, 
+				"WHEN the date was entered in Calendar dropdown, THEN you SHOULD see date in the Input Field");
+	}
+	
+	@Test(dependsOnMethods = { "typeDateInputTest" })
+	public void typeTimeTest() throws InterruptedException{
+		SoftAssert testTime = new SoftAssert();
+		WebElement input_hour = driver.findElement(By.id("InputHour"));
+		input_hour.click();
+		Select dropdown_hour = new Select(driver.findElement(By.id("InputHour")));
+		dropdown_hour.selectByValue(UtilInput.hour);
+		WebElement input_min = driver.findElement(By.id("InputMinute"));
+		input_min.click();
+		Select dropdown_min = new Select(driver.findElement(By.id("InputMinute")));
+		dropdown_min.selectByValue(UtilInput.min);
+		WebElement input_meridian = driver.findElement(By.id("InputMeridian"));
+		input_meridian.click();
+		Select dropdown_meridian = new Select(driver.findElement(By.id("InputMeridian")));
+		dropdown_meridian.selectByValue(UtilInput.meridian);
+		testTime.assertEquals(dropdown_hour.getFirstSelectedOption().getText(), 
+				UtilInput.hour, "WHEN the hour is inputted, THEN it SHOULD display the same hour.");
+		testTime.assertEquals(dropdown_min.getFirstSelectedOption().getText(), 
+				UtilInput.min, "WHEN the minute is inputted, THEN it SHOULD display the same minute.");
+		testTime.assertEquals(dropdown_meridian.getFirstSelectedOption().getText(), 
+				UtilInput.meridian, "WHEN the meridian is inputted, THEN it SHOULD display the same meridian.");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+	
+	public void wait_for_start_btn() {
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("NextButton")));
+	}
+	
 }
